a35ab10c701c9b5cbf11d06a7c0a371825c0ed3f
Mark D. Butala <butala@illinois.edu>
2016-10-01 00:54:11 -0500

    Changes required for setuptools.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='pyviz3d',
+      version='0.1',
+      description='Python 3-D visualization.',
+      author='Mark D. Butala',
+      author_email='butala@illinois.edu',
+      packages = ['pyviz3d'])
