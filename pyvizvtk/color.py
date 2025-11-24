1b53c094bc7f93ae57c7d37122e31a2c8d368fbc
Mark D. Butala <markbutala@intl.zju.edu.cn>
2025-11-24 16:53:30 +0800

    Change to colors with 1 as max instead of 255.

--- pyvizvtk/color.py
+++ pyvizvtk/color.py
@@ -4,4 +4,4 @@ import matplotlib.colors
 def get_color(name):
     """
     """
-    return tuple([int(x * 255) for x in matplotlib.colors.to_rgb(name)])
+    return tuple([int(x) for x in matplotlib.colors.to_rgb(name)])
